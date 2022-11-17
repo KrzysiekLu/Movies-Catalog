3a4c89b278ff2a2ac92fcae0a197b91965ad3cf8
KrzysiekLu <krzysztof.luuczak@gmail.com>
2022-11-17 17:11:22 +0100

    adding pages navigation

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const API_KEY = "b1faa3b7206840f87e88b9254b08fa74";
 const container = document.querySelector(".films-container");
 const categorySelect = document.querySelector(".category-input");
 
+// Pages Navigation
+const pageNavBtns = document.querySelectorAll(".page-nav__btn");
+const curPage = document.querySelector(".pages-nav__current-page");
+
 const createSelectInput = (catArr) => {
   catArr.forEach((cat) => {
     const catOption = document.createElement("option");
@@ -9,6 +13,13 @@ const createSelectInput = (catArr) => {
     catOption.value = cat.id;
     categorySelect.appendChild(catOption);
   });
+};
+
+// navigation data store
+const navigationData = {
+  currentPage: 1,
+  numOfPages: null,
+  filmCategory: 28,
 };
 
 const getCategories = async () => {
@@ -20,36 +31,42 @@ const getCategories = async () => {
     const catList = catListData.genres;
     createSelectInput(catList);
   } catch (error) {
-    alert("Problem with get category list");
+    console.error(error);
   }
 };
 getCategories();
 
-const getFilms = async (id) => {
+const getFilms = async (id, pageNum = 1) => {
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/4/discover/movie?api_key=${API_KEY}&with_genres=${id}&language=pl-PL&page=1`
+      `https://api.themoviedb.org/4/discover/movie?api_key=${API_KEY}&with_genres=${id}&language=pl-PL&page=${pageNum}`
     );
     const films = await response.json();
-    console.log(films.total_pages);
+
+    navigationData.currentPage = films.page;
+    navigationData.numOfPages = films.total_pages;
+    console.log(films);
 
     return films;
   } catch (error) {
-    alert("Problem with get films");
+    console.error(error);
   }
 };
 
-const displayPosters = async (filmID) => {
+export const displayPosters = async (filmID, pageNum) => {
   try {
     let urlBase = `https://image.tmdb.org/t/p/w500/`;
-    const { results } = await getFilms(filmID);
+    const { results } = await getFilms(filmID, pageNum);
     results.forEach((movie) => {
       const poster = document.createElement("img");
       poster.src = `${urlBase}${movie.poster_path}`;
       container.insertAdjacentElement("beforeend", poster);
+      const infoWraper = document.createElement("div");
+      infoWraper.classList.add("film-info");
+      poster.insertAdjacentElement("beforeend", infoWraper);
     });
   } catch (error) {
-    alert("Problem with geting posters");
+    console.error(error);
   }
 };
 
@@ -59,5 +76,26 @@ window.addEventListener("DOMContentLoaded", () => {
 
 categorySelect.addEventListener("change", (e) => {
   container.textContent = "";
+  navigationData.filmCategory = e.target.value;
+  navigationData.currentPage = 1;
+  curPage.textContent = navigationData.currentPage;
+
   displayPosters(e.target.value);
+  return e.target.value;
 });
+
+pageNavBtns.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    e.target.classList.contains("back")
+      ? navigationData.currentPage > 1
+        ? navigationData.currentPage--
+        : navigationData.currentPage
+      : navigationData.currentPage < navigationData.numOfPages
+      ? navigationData.currentPage++
+      : navigationData.currentPage;
+
+    curPage.textContent = navigationData.currentPage;
+    container.textContent = "";
+    displayPosters(navigationData.filmCategory, navigationData.currentPage);
+  })
+);
